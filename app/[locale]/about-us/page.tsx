c152c02afe9f36b0533c1f10f8fbb86fd76333c4
Salah Mohamed <salah.mohamed0404@gmail.com>
2025-02-07 11:43:40 +0200

    fix: update AboutUs component to use locale from props instead of cookies

--- app/[locale]/about-us/page.tsx
+++ app/[locale]/about-us/page.tsx
@@ -1,14 +1,17 @@
 import initTranslations from "../i18n";
-import { cookies } from "next/headers";
 import Hero from "@app/_components/About/Hero";
 import OurValues from "@app/_components/About/OurValues";
 import Testimonials from "@app/_components/About/Testimonials";
 import ScreenHeader from "@app/_components/Common/ScreenHeader";
 import { defaultLocale } from "@/constants";
 
-async function AboutUs() {
-  const lang = cookies().get("lang")?.value || defaultLocale;
-  const { t } = await initTranslations(lang);
+type AboutUsProps = {
+  params: { locale: string };
+};
+
+async function AboutUs({ params: { locale } }: AboutUsProps) {
+  const { t } = await initTranslations(locale || defaultLocale);
+
   return (
     <main className="animate-fade-left">
       <ScreenHeader
