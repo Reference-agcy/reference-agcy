d075e1e15179926d71138074f7c213ce8090cd4f
Salah Mohamed <salah.mohamed0404@gmail.com>
2025-02-07 11:02:19 +0200

    feat: implement navigation drawer state management on path change

--- app/_components/Common/NavBar/NavDrawer.tsx
+++ app/_components/Common/NavBar/NavDrawer.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import { useEffect, useRef, useState } from "react";
+import { usePathname } from "next/navigation";
 import { HiMenuAlt3 } from "react-icons/hi";
 import { IoClose } from "react-icons/io5";
 import Logo from "@app/_components/Common/Logo";
@@ -14,8 +16,22 @@ import MainModules from "./MainModules";
 import Others from "./Others";
 
 function NavDrawer() {
+  const [open, setOpen] = useState(false);
+  const pathname = usePathname();
+  const previousPathRef = useRef(pathname);
+
+  useEffect(() => {
+    const currentPath = pathname;
+    const previousPath = previousPathRef.current;
+
+    if (currentPath !== previousPath) {
+      setOpen(false);
+    }
+    previousPathRef.current = pathname;
+  }, [pathname]);
+
   return (
-    <Drawer direction="left">
+    <Drawer direction="left" open={open} onOpenChange={setOpen}>
       <DrawerTrigger title="Open navigation menu" className="rtl:-scale-x-100">
         <HiMenuAlt3 className="size-6" />
       </DrawerTrigger>
