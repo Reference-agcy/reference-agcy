56d4cc48c5501fb634d8a0022b15754e8aacee1b
Salah Mohamed <salah.mohamed0404@gmail.com>
2025-02-21 15:10:20 +0200

    fix: comment out unused job fetching logic in JobsList component

--- app/_components/JopOpening/JobsList.tsx
+++ app/_components/JopOpening/JobsList.tsx
@@ -7,20 +7,20 @@ import { useGetJobs } from "./useGetJobs";
 import useFetchNextPageProps from "@app/_hooks/useFetchNextPage";
 
 const JobsList = () => {
-  const { data, isFetchingNextPage, isPending, hasNextPage, fetchNextPage } =
-    useGetJobs();
-  useFetchNextPageProps({
-    isFetchingNextPage,
-    hasNextPage,
-    fetchNextPage,
-  });
-  const jobs = data?.pages.flatMap((page) => page.data) || [];
+  // const { data, isFetchingNextPage, isPending, hasNextPage, fetchNextPage } =
+  //   useGetJobs();
+  // useFetchNextPageProps({
+  //   isFetchingNextPage,
+  //   hasNextPage,
+  //   fetchNextPage,
+  // });
+  // const jobs = data?.pages.flatMap((page) => page.data) || [];
   return (
     <section data-aos="fade-up">
       <h2 className="mb-6 text-4xl font-bold tracking-tighter text-gray-900">
         Full-Time Jobs
       </h2>
-      <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-3">
+      {/* <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-3">
         {isPending ? (
           <div className="col-span-full flex justify-center px-8 py-6">
             <Spinner />
@@ -33,7 +33,7 @@ const JobsList = () => {
             <Spinner />
           </div>
         )}
-      </div>
+      </div> */}
     </section>
   );
 };
