2648a4141a4311135cc2ec4a33589907c1072e56
Salah Mohamed <salah.mohamed0404@gmail.com>
2025-03-15 14:13:35 +0200

    feat: add ChangePassword page to admin for user password updates

---
+++ app/admin/(adminPages)/change-password/page.tsx
@@ -0,0 +1,11 @@
+import ChangePasswordCard from "@app/_components/Admin/ChangePasswordCard";
+
+function ChangePassword() {
+  return (
+    <div className="flex h-full items-center justify-center">
+      <ChangePasswordCard />
+    </div>
+  );
+}
+
+export default ChangePassword;
