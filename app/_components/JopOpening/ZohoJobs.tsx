5389e1d16f2028f0337a14bb7e6ed10978da3ce1
Salah Mohamed <salah.mohamed0404@gmail.com>
2025-04-09 19:14:46 +0200

    Enhance ZohoJobs component: implement reload logic and update job listing styles

--- app/_components/JopOpening/ZohoJobs.tsx
+++ app/_components/JopOpening/ZohoJobs.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import { useEffect } from "react";
+
 // styled zoho job openings embedded code
 const ZOHO_EMBEDDED_CODE = `
 		<link
@@ -10,7 +12,7 @@ const ZOHO_EMBEDDED_CODE = `
 		<div class="embed_jobs_head embed_jobs_with_style_3 embed_jobs_with_style min-h-[55dvh] !bg-transparent !shadow-none">
 			<div class="embed_jobs_head2 min-h-[55dvh] rounded-2xl !border-none shadow">
 				<div class="embed_jobs_head3">
-					<div id="rec_job_listing_div" class="[&>div>div]:!rounded-[8px]"></div>
+					<div id="rec_job_listing_div" class="[&>div>div]:!rounded-[8px] [&_a]:!text-primary-600"></div>
 					<script
 						type="text/javascript"
 						src="https://static.zohocdn.com/recruit/embed_careers_site/javascript/v1.1/embed_jobs.js"
@@ -30,6 +32,24 @@ const ZOHO_EMBEDDED_CODE = `
     `;
 
 function ZohoJobs() {
+  useEffect(() => {
+    // Check if this is the first load
+    const hasReloaded = localStorage.getItem("zohoJobsReloaded");
+
+    if (!hasReloaded) {
+      // Set the flag before reloading
+      localStorage.setItem("zohoJobsReloaded", "true");
+      // Reload once
+      window.location.reload();
+    } else {
+      // Clear the flag after some time to allow future reloads if needed
+      // e.g., when user navigates away and comes back
+      setTimeout(() => {
+        localStorage.removeItem("zohoJobsReloaded");
+      }, 5000);
+    }
+  }, []);
+
   return (
     <div
       id="zoho-jobs"
